1e258a215a3820cf58befb652e9b837768f426b8
Tom Linforth <tom.linforth1@gmail.com>
2020-02-18 10:15:45 +0000

    fixed csrf temp

--- ufluentBE/server/views.py
+++ ufluentBE/server/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
+@csrf_exempt
 def userByUsername(request, username):
   if request.method == "POST":
     print(username) 
