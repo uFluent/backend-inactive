652d4a6336ea573ac22916f2841977655ae5f14a
carlos <carlos.beltran.exposito@gmail.com>
2020-02-18 10:06:48 +0000

    prod data

--- ufluentBE/ufluentBE/settings.py
+++ ufluentBE/ufluentBE/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'schema'
+    'schema',
+    'server'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
