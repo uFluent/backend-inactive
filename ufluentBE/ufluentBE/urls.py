f5608d3936ddb3cd480a6d6bc68693f639d2e17c
Tom Linforth <tom.linforth1@gmail.com>
2020-02-18 10:02:26 +0000

    started server, dummy endpoint created

--- ufluentBE/ufluentBE/urls.py
+++ ufluentBE/ufluentBE/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from server import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^users/(?P<username>\w+)/$', views.userByUsername)
 ]
